64341a60afb1f02294075c2b9a97357773202ee1
Rafay <Rafay>
2022-07-06 23:37:55 +0500

    Updating the Install-Script

--- install-apache.sh
+++ install-apache.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-yum update -y
-yum install -y httpd.x86_64
-systemctl start httpd.service
+#yum update -y
+#yum install -y httpd.x86_64
+#systemctl start httpd.service
+#systemctl enable httpd.service
+#echo Hello World from $(hostname -f) > /var/www/html/index.html
+
+sudo yum install httpd -y
+sudo yum install php -y
+sudo systemctl start httpd
+sudo systemctl start php
 systemctl enable httpd.service
-echo "Hello World from $(hostname -f)" > /var/www/html/index.html
+systemctl enable httpd.service
+cd /var/www/html
+sudo wget https://wordpress.org/latest.zip
+sudo unzip latest.zip
